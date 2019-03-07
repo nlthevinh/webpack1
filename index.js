b8b69c9fab07fbbe9c872520fd220855ec65beca
nlthevinh <46483319+nlthevinh@users.noreply.github.com>
2019-03-07 08:46:57 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+//const hello = 'world';
+//const.log(hello);
+
+function component() {
+    let element = document.createElement('div');
+  
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
